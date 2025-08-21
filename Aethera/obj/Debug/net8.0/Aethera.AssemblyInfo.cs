d3ccdf8f38a965a1a3c3ca0e8fff35f83f78f2b5
maria vassilikopoulou <maria.vassilikopoulou@iths.se>
2025-08-21 14:20:52 +0200

    add yml

--- Aethera/obj/Debug/net8.0/Aethera.AssemblyInfo.cs
+++ Aethera/obj/Debug/net8.0/Aethera.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aethera")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db715c7b4a5570c549af1f623ee3de96342c86fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7dc896cda431a1d866ca0a3082cff7ac774905e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aethera")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aethera")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
